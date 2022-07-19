204f1c3b2f0d904247bed62c2e9788efd70409ec
Allison Zou <allison.artemis.zou@gmail.com>
2022-07-19 16:40:30 -0400

    Add pseudocode implementation plan

---
+++ app.js
@@ -0,0 +1,19 @@
+// TODO: list rock, paper, scissors
+function getComputerChoice() {
+  // TODO: randomly select between rock, paper scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+  // TODO: define win-lose relationship between rock-paper-scissors
+  // TODO: Return either "player" or "computer" based on winner
+}
+
+function game(numRounds) {
+  // TODO: stores wins for both computer and player
+  // TODO: loop for numRounds
+    // TODO: get player prompt for rps
+    // TODO: check for correctness, case-insensitive
+
+  // TODO: declare final winner
+}
+
